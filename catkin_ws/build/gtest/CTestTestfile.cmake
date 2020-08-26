17abf65940486bf38a3d0f7675626bb8a117823d
noidname01 <tim983649@gmail.com>
2020-08-26 15:56:01 +0800

    Test.js

--- catkin_ws/build/gtest/CTestTestfile.cmake
+++ catkin_ws/build/gtest/CTestTestfile.cmake
@@ -1,6 +1,6 @@
 # CMake generated Testfile for 
 # Source directory: /usr/src/googletest
-# Build directory: /home/noidname/UV_Robotic_Challenge-Software/catkin_ws/build/gtest
+# Build directory: /home/pi/UV_Robotic_Challenge-Software/catkin_ws/build/gtest
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
--- catkin_ws/devel/share/uv_robot_ros/cmake/uv_robot_ros-msg-extras.cmake
+++ catkin_ws/devel/share/uv_robot_ros/cmake/uv_robot_ros-msg-extras.cmake
@@ -1,2 +1,2 @@
 set(uv_robot_ros_MESSAGE_FILES "")
-set(uv_robot_ros_SERVICE_FILES "/home/noidname/UV_Robotic_Challenge-Software/catkin_ws/src/uv_robot_ros/srv/cmdToRpi.srv")
+set(uv_robot_ros_SERVICE_FILES "/home/pi/UV_Robotic_Challenge-Software/catkin_ws/src/uv_robot_ros/srv/cmdToRpi.srv")
