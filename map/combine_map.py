7131168d5a13405e917ed3f4633d9f729b092264
hsiang20 <h511168@nehs.hc.edu.tw>
2020-08-26 22:21:19 +0800

    combine map;

---
+++ map/combine_map.py
@@ -0,0 +1,61 @@
+with open('obstacle_smooth.txt', 'r') as f:
+    lines = [line.strip() for line in f.readlines()]
+obstacle_range_x = int(lines[-1].split()[0])
+obstacle_range_y = int(lines[-1].split()[1])
+obstacle_origin_x = int(lines[-2].split()[0])
+obstacle_origin_y = int(lines[-2].split()[1])
+obstacle = [[] for i in range(obstacle_range_x)]
+for i in range(obstacle_range_x):
+    for j in range(obstacle_range_y):
+        obstacle[i].append(int(lines[i][j]))
+
+with open('odometry_path.txt', 'r') as f:
+    lines = [line.strip() for line in f.readlines()]
+odometry_range_x = int(lines[-1].split()[0])
+odometry_range_y = int(lines[-1].split()[1])
+odometry_origin_x = int(lines[-2].split()[0])
+odometry_origin_y = int(lines[-2].split()[1])
+odometry = [[] for i in range(odometry_range_x)]
+for i in range(odometry_range_x):
+    for j in range(odometry_range_y):
+        odometry[i].append(int(lines[i][j]))
+
+combine_range_x = max(obstacle_range_x, odometry_range_x)
+combine_range_y = max(obstacle_range_y, odometry_range_y)
+combine_origin_x = max(obstacle_origin_x, odometry_origin_x)
+combine_origin_y = max(obstacle_origin_y, odometry_origin_y)
+combine = [[] for i in range(combine_range_x)]
+for i in range(combine_range_x):
+    for j in range(combine_range_y):
+        combine[i].append(0)
+print('obs_r_x:', obstacle_range_x)
+print('obs_r_y:', obstacle_range_y)
+print('obs_o_x:', obstacle_origin_x)
+print('obs_o_y:', obstacle_origin_y)
+print('odom_r_x:', odometry_range_x)
+print('odom_r_y:', odometry_range_y)
+print('odom_o_x:', odometry_origin_x)
+print('odom_o_y:', odometry_origin_y)
+print('com_r_x:', combine_range_x)
+print('com_r_y:', combine_range_y)
+print('com_o_x:', combine_origin_x)
+print('com_o_y:', combine_origin_y)
+
+
+for i in range(obstacle_range_x):
+    for j in range(obstacle_range_y):
+        combine[i+combine_origin_x-obstacle_origin_x][j+combine_origin_y-obstacle_origin_y] += obstacle[i][j]
+for i in range(odometry_range_x):
+    for j in range(odometry_range_y):
+        combine[i+combine_origin_x-odometry_origin_x][j+combine_origin_y-odometry_origin_y] += odometry[i][j]
+
+with open('combine_map.txt', 'w') as f:
+    for i in range(combine_range_x):
+        for j in range(combine_range_y):
+            f.write(str(combine[i][j]))
+        f.write('\n')
+    f.write(str(combine_origin_x)+' '+str(combine_origin_y)+'\n')
+    f.write(str(combine_range_x)+' '+str(combine_range_y))
+
+
+
---
+++ map/obstacle.py
@@ -0,0 +1,166 @@
+import os
+
+filename = None
+def pcd_exist():
+    global filename
+    path = os.getcwd()
+    name_list = os.listdir(path)
+    for name in name_list:
+        try:
+            if name[-3:] == 'pcd': 
+                filename = name
+                return True
+        except:
+            pass
+    return False
+
+while pcd_exist():
+    with open(filename, 'r') as f:
+        lines = [line.strip().split() for line in f.readlines()]
+    os.remove(filename)
+    is_data = False
+    data = []
+    newlines = []
+    for line in lines:
+        if is_data == False:
+            data.append(line)
+        if line[0] == 'DATA':
+            is_data = True
+            continue
+        if is_data == True:
+            if float(line[2]) < 1.6:
+                line[2] = str(0)
+                newlines.append(line)
+    count = len(newlines)
+    with open('obstacle.pcd', 'w') as f:
+        for line in data:
+            for element in line:
+                if element == 'POINTS' or element == 'WIDTH':
+                    f.write(element)
+                    f.write(' ')
+                    f.write(str(count))
+                    break
+                f.write(element)
+                f.write(' ')
+            f.write('\n')
+        for line in newlines:
+            for element in line:
+                f.write(element)
+                f.write(' ')
+            f.write('\n')
+    
+    thres = 5
+    with open('obstacle.pcd', 'r') as f:
+        lines = [line.strip().split() for line in f.readlines()]
+    info = []
+    data = []
+    is_data = False
+    for line in lines:
+        if is_data == False:
+            info.append(line)
+        if line[0] == 'DATA':
+            is_data = True
+            continue
+        if is_data == True:
+            data.append(line)
+
+    min_x = 100
+    max_x = -100
+    min_y = 100
+    max_y = -100
+    for d in data:
+        if float(d[0]) < min_x:
+            min_x = float(d[0])
+        if float(d[0]) > max_x:
+            max_x = float(d[0])
+        if float(d[1]) < min_y:
+            min_y = float(d[1])
+        if float(d[1]) > max_y:
+            max_y = float(d[1])
+
+    for d in data:
+        d[0] = float(d[0]) - min_x
+        d[1] = float(d[1]) - min_y
+
+    total_x = (max_x - min_x) * 10
+    total_y = (max_y - min_y) * 10
+    range_x = int(total_x//1) + 1
+    range_y = int(total_y//1) + 1
+    #print(range_x, range_y)
+
+    dot_dict = dict()
+    for i in range(range_x):
+        for j in range(range_y):
+            dot_dict[(i, j)] = 0
+    for d in data:
+        dot_dict[(int(d[0]*10//1),int(d[1]*10//1))] += 1
+
+
+    with open('obstacle.txt', 'w') as f:
+        #f.write('P2\n')
+        #f.write('15\n')
+        for i in range(range_x):
+            for j in range(range_y):
+                if dot_dict[(i, j)] > 0:
+                    f.write(str('3'))
+                else:
+                    f.write(str('0'))
+                #print(dot_dict[(i, j('0')])
+            f.write('\n')
+        f.write(str(int(-min_x*10//1))+' '+str(int(-min_y*10//1))+'\n')
+        f.write(str(range_x)+' '+str(range_y))
+#smooth
+    for i in range(range_x):
+        for j in range(range_y):
+            count = 0
+            if i>0 and j>0 and i<range_x-2 and j<range_y-2:
+                if dot_dict[((i-1), (j-1))] > thres:
+                    count += 1
+                else:
+                    count -= 1
+                if dot_dict[((i-1), j)] > thres:
+                    count += 1
+                else:
+                    count -= 1
+                if dot_dict[(i, (j-1))] > thres:
+                    count += 1
+                else:
+                    count -= 1
+                if dot_dict[((i+1), (j+1))] > thres:
+                    count += 1
+                else:
+                    count -= 1
+                if dot_dict[((i+1), j)] > thres:
+                    count += 1
+                else:
+                    count -= 1
+                if dot_dict[(i, (j+1))] > thres:
+                    count += 1
+                else:
+                    count -= 1
+                if dot_dict[((i-1), (j+1))] > thres:
+                    count += 1
+                else:
+                    count -= 1
+                if dot_dict[((i+1), (j-1))] > thres:
+                    count += 1
+                else:
+                    count -= 1
+                dot_dict[(i, j)] += count*5
+
+
+#/smooth
+
+    with open('obstacle_smooth.txt', 'w') as f:
+        #f.write('P2\n')
+        #f.write('15\n')
+        for i in range(range_x):
+            for j in range(range_y):
+                if dot_dict[(i, j)] > 0:
+                    f.write(str('3'))
+                else:
+                    f.write(str('0'))
+                #print(dot_dict[(i, j('0')])
+            f.write('\n')
+        f.write(str(int(-min_x*10//1))+' '+str(int(-min_y*10//1))+'\n')
+        f.write(str(range_x)+' '+str(range_y))
